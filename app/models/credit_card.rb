97c5999c4d9eeeb8969bd3f09ddb39e0552c5d88
Raelen Marchetti <raelenmarchetti@gmail.com>
2023-05-27 16:08:42 -0400

    validates added

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,3 +1,7 @@
 class CreditCard < ApplicationRecord
   belongs_to :user
+
+  validates :card_number, presence: true
+  validates :expiration_date, presence: true
+  validates :cvv, presence: true
 end
