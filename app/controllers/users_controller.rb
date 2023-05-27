b863bd564ba937bfd138326c3bf5c197d84d4b0e
Raelen Marchetti <raelenmarchetti@gmail.com>
2023-05-27 17:44:09 -0400

    add views for testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,28 @@
 class UsersController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
-    def index
-    end
+  def index
+  end
 
-    def edit
-      @user = User.find(params[:id])
-    end
+  def show
+    @user = User.find(params[:id])
+  end
 
-    def update
-      if @user.update(user_params)
-        redirect_to edit_user_path, notice: 'User information was successfully updated.'
-      else
-        render :edit
-      end
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to edit_user_path, notice: 'User information was successfully updated.'
+    else
+      render :edit
     end
+  end
 
     private
 
-    def user_params
-      params.require(:user).permit(:name, :address, :phone_number)
-    end
+  def user_params
+    params.require(:user).permit(:name, :address, :phone_number)
   end
+end
