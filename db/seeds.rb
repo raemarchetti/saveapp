b459ecb9ea21ab7bace885184868da09f2b015c5
Mariana Doldan <marianamilanid@gmail.com>
2023-05-27 16:58:05 -0300

    creditcard faker ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# ...
+
+user = User.first
+
+credit_card_data = {
+  card_number: Faker::Stripe.valid_card,
+  card_holder_name: user.name,
+  expiration_date: Faker::Date.between(from: Date.today, to: 10.years.from_now).strftime('%m/%y'),
+  cvv: Faker::Stripe.ccv
+}
+
+credit_card = user.credit_cards.create(credit_card_data)
