61de725d44fb1da24361292c14a995f5820197a7
tiago conti <tgconti@gmail.com>
2023-05-27 11:35:53 -0300

    modified method next_value

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,14 +5,13 @@ class Transaction < ApplicationRecord
   # method to calculate the rouded-up value
 
   def next_value
-    if @transaction_amount.positive?
-      next_integer = @transaction_amount.ceil
-      value_added = next_integer - @transaction_amount
-    elsif @transaction_amount.negative?
-      next_integer = @transaction_amount.floor
-      value_added = @transaction_amount - next_integer
+    @transction = Transaction.transaction_amount
+    if @transaction.positive?
+      next_integer = @transaction.ceil
+      value_added = next_integer - @transaction
     else
-      return [0, 1]
+      next_integer = @transaction.floor
+      value_added = @transaction - next_integer
     end
     return [value_added.round(2), next_integer]
   end
