bc00c5c40fc136e949a8dc80d11474b6e98a0efc
Mariana Doldan <marianamilanid@gmail.com>
2023-05-27 18:52:31 -0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,43 +7,49 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 require 'faker'
-Transaction.destroy_all
-CreditCard.destroy_all
-User.destroy_all
 
-admin = User.create!(
-  email: "test@admin.test",
-  password: 'password',
-  name: 'Jane Doe',
-  address: Faker::Address.unique.full_address,
-  phone_number: Faker::PhoneNumber.unique.cell_phone_in_e164
-)
+if Rails.env.development?
 
-user = User.create!(
-  email: Faker::Internet.email,
-  password: 'password',
-  name: Faker::Name.name,
-  address: Faker::Address.unique.full_address,
-  phone_number: Faker::PhoneNumber.unique.cell_phone_in_e164
-)
+  Transaction.destroy_all
+  CreditCard.destroy_all
+  Goal.destroy_all
+  User.destroy_all
 
-credit_card = CreditCard.create!(
-  card_number: Faker::Stripe.valid_card,
-  card_holder_name: user.name,
-  expiration_date: Faker::Date.between(from: Date.today, to: 10.years.from_now).strftime('%m/%y'),
-  cvv: Faker::Stripe.ccv,
-  user_id: user.id
+  user = User.create!(
+    email: "test@admin.test",
+    password: 'password',
+    name: 'Jane Doe',
+    address: Faker::Address.unique.full_address,
+    phone_number: Faker::PhoneNumber.unique.cell_phone_in_e164
   )
 
+  # user = User.create!(
+  #   email: Faker::Internet.email,
+  #   password: 'password',
+  #   name: Faker::Name.name,
+  #   address: Faker::Address.unique.full_address,
+  #   phone_number: Faker::PhoneNumber.unique.cell_phone_in_e164
+  #  )
 
+  credit_card = CreditCard.create!(
+    card_number: Faker::Stripe.valid_card,
+    card_holder_name: user.name,
+    expiration_date: Faker::Date.between(from: Date.today, to: 10.years.from_now).strftime('%m/%y'),
+    cvv: Faker::Stripe.ccv,
+    user_id: user.id
+    )
 
-10.times do
-  transaction = Transaction.create!(
-   user: User.first,
-   transaction_amount: Faker::Commerce.price(range: 10.0..100.0),
-   transaction_date: Faker::Date.between(from: 1.year.ago, to: Date.today),
-   transaction_origin: Faker::Company.name,
-   goal: goal.goal_name,
-   user_id: user.id
- )
+  goal = [
+     Goal.create!(goal_name: 'travel', user: user, goal_amount: Faker::Number.between(from: 200.0, to: 3000.0), goal_balance: 0)
+  ]
+
+  10.times do
+    transaction = Transaction.create!(
+    user: User.first,
+    transaction_amount: Faker::Commerce.price(range: 10.1..299.99),
+    transaction_date: Faker::Date.between(from: 1.year.ago, to: Date.today),
+    transaction_origin: Faker::Company.name,
+    goal_id: Goal.first.id
+  )
+  end
 end
