ce8f875279ab08839aa9c0e66b9559fdc9d83466
Raelen Marchetti <raelenmarchetti@gmail.com>
2023-05-31 20:14:50 -0400

    css added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get "profile", to: "pages#profile", as: :profile
+  get "welcome", to: "pages#welcome", as: :welcome
 
   resources :goals
   resources :credit_cards, except: :index
