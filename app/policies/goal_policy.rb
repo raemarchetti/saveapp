fab2814293d4b15c706657ccc919f4cca3b149ee
Mariana Doldan <marianamilanid@gmail.com>
2023-06-03 11:53:32 -0300

    done & reviewed

--- app/policies/goal_policy.rb
+++ app/policies/goal_policy.rb
@@ -2,11 +2,7 @@ class GoalPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
-      scope.where(user:user)
-    end
-
-    def new
-      true
+      scope.where(user: user)
     end
   end
 end
