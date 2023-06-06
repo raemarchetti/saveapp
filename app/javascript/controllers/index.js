7bf56dbef58cdc6f41c0dafb7c4320bdae715fb6
Raelen Marchetti <raelenmarchetti@gmail.com>
2023-06-06 19:55:53 -0400

    home page fix

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -10,7 +10,7 @@ application.register("hello", HelloController)
 const upBtn = document.querySelector(".up-button");
 const downBtn = document.querySelector(".down-button");
 const sidebar = document.querySelector(".sidebar");
-const container = document.querySelector(".container");
+const container = document.querySelector(".container-page");
 const mainSlide = document.querySelector(".main-slide");
 const slideCount = mainSlide.querySelectorAll("div").length;
 
