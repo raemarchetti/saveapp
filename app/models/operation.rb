aa6051f5f2fbca93ea0b3941dae14fe0361f09b7
tiago conti <tgconti@gmail.com>
2023-06-06 15:50:09 -0300

    next_valeu rounds to 1 case

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -7,11 +7,12 @@ class Operation < ApplicationRecord
     if @value > 0
       next_integer = @value.ceil
       value_added = next_integer - @value
+      value_added = 1 if value_added.zero?
     else
       next_integer = @value.floor
       value_added = @value - next_integer
     end
-    [value_added.round(2), next_integer]
+    [value_added, next_integer]
   end
 
   validates :operation_amount, presence: true
